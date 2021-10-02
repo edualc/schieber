eac19085f069c7580daf7f2a7bdedf941ca1cd4f
Claude Lehmann <e.dualc@hotmail.com>
2021-10-02 18:24:57 +0200

    add readme text and image

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 init:
 	pip install -r requirements.txt
 
+run:
+	python3 schieber/main.py
+
 test:
 	pytest tests
---
+++ schieber/main.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import List
+
+
+import code; code.interact(local=dict(globals(), **locals()))
